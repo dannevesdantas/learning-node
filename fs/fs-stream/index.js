5461bc0b2b6b68ed9e180cba294e7a3e91aad361
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2024-01-07 03:24:53 -0300

    Create fs-stream

---
+++ fs/fs-stream/index.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+var readable = fs.createReadStream('./working.txt').setEncoding('utf8');
+var data = '';
+
+readable.on('data', function (chunk) {
+    data += chunk;
+});
+
+readable.on('end', function () {
+    console.log(data);
+});
